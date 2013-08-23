d32b5a8c4f7178f221b43629e0f0fbed3f282af3
Kevin Quach <kevin.quach@gmail.com>
2013-08-23 14:58:02 -0400

    add totals for income,expenses, overdrafts

--- tracker.rb
+++ tracker.rb
@@ -5,23 +5,35 @@ require 'csv'
 #Balance starts at 0.00, the amount of each transaction is added to the balance in order
 @balance = 0
 @total_income = 0
-@total_expenses = 0
 @overdraft = 0
+@balance_overdraft = []
+
 
 #Read csv file into array
 CSV.foreach('transactions.csv', headers: true) do |row|
   @balance += row[1].to_f
 
-  puts "Date: #{row[0]}"
-  puts "Amount: #{row[1]}"
-  puts "Description: #{row[2]}"
-  puts "Balance: #{@balance.round(2)}"
+  print "Amount: #{row[1]} | "
+    if row[1].to_f > 0
+      @total_income += row[1].to_f
+    end
+  print "Description: #{row[2]} | "
+  print "Date: #{row[0]} | "
+    if @balance < 0
+      print "OVERDRAFT |"
+      @overdraft += 20
+      @balance -= 20
+      @balance_overdraft << @balance.round(2)
+      @balance_overdraft << row[1]
+    end
+  print "Balance: #{@balance.round(2)}"
+
   puts
 
 end
   #If balance < 0
     # Charge overdraft fee of 20.00
-    @overdraft += 20
+
   #Else do nothing
 
 #Print financial summary
@@ -30,9 +42,16 @@ end
   #Print total income after processing all transactions
   puts "Total income: #{@total_income}"
   #Print total expenses after processing all transactions
-  puts "Total expenses: #{@total_expenses}"
-
+  puts "Total expenses: #{(@total_income - @balance).round(2)}"
+  #Print total overdraft
+  puts "Total overdraft: #{@overdraft}"
 #Print overdraft summary
   #Print total overdraft charges
+
+  puts "Overdrafts (balance, expense, description, date)"
+  puts @balance_overdraft
+
   #Print the heading after the total overdraft charges
   #For each transaction that caused overdraft print current balance, transaction amount, description, and date on separate line
+
+
